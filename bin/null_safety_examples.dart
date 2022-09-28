3bf94a04636ef4520b7d24805f3395b06e2697e2
Mahmoud Habib <mahmoudhabib.eng@gmail.com>
2022-09-28 08:05:56 +0200

    init repo

---
+++ bin/null_safety_examples.dart
@@ -0,0 +1,5 @@
+import 'package:null_safety_examples/null_safety_examples.dart' as null_safety_examples;
+
+void main(List<String> arguments) {
+  print('Hello world: ${null_safety_examples.calculate()}!');
+}
